e045366b07ca7c18bcfd8fe923f2193d23c1946b
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2022-11-01 13:43:17 -0400

    add ProfileViewer-useEffect

---
+++ src/TvForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const TvForm = () => {
+    return(<div></div>)
+}
+
+export default TvForm;
+
---
+++ src/TvMovie.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const TvMovie = () => {
+    return(<></>)
+}
+
+export default TvMovie;
+
